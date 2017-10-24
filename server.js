df3dd57992cd89226241a656963dce6d006ce723
Vinnie Chen <vinnie.chen211@gmail.com>
2017-10-23 19:16:04 -0700

    Set up node.js and first push to heroku, added sample get request call

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+// API Endpoints 
+app.get("/createUser", function(req, res) {
+    res.send({
+        "user" : "test_user",
+        "isMaker" : true,
+        "shortBio" : "this is the shorter bio.",
+        "age" : 21,
+        "profiles" : {
+            "maker" : {
+              longBio: "this is the maker long bio",  
+                photos: "",
+                icon: true,
+            },
+            "backer" : {
+             longBio: "this is the maker long bio",  
+                photos: "",
+                icon: true,   
+            }
+        }
+             });
+})
