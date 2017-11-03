f6b98eee73fe0b61a31ba29ed5c6cefa472806ea
bpaw <bpaw.dev@gmail.com>
2017-11-02 17:30:00 -0700

    put important stuff back

--- routes/user.js
+++ routes/user.js
@@ -11,6 +11,20 @@ router.post('/addNew', function(req, res) {
 // POST request to create user in database
 router.post("/", function(req, res) {
 
+  var name = req.body.name;
+  var age = req.body.age;
+  var email = req.body.email;
+  var isMaker = req.body.isMaker;
+  var shortBio = req.body.shortBio;
+
+  var makerLongBio = req.body.profiles.maker.longBio
+  var makerPhotos = req.body.profiles.maker.photos
+  var makerIcon = req.body.profiles.maker.icons
+
+  var backerLongBio = req.body.profiles.backer.longBio
+  var backerPhotos = req.body.profiles.backer.photos
+  var backerIcon = req.body.profiles.backer.icons
+
   db.createUser(name,age,email,isMaker,"users", database);
   db.createUserProfile(makerLongBio,makerPhotos,makerIcon,email,"maker", database)
   db.createUserProfile(backerLongBio,backerPhotos,backerIcon,email,"backer", database)
@@ -22,7 +36,7 @@ router.post("/", function(req, res) {
 router.get('/', function(req, res) {
   var email = req.body.email
   var user = db.readUser(email, 'users', database, function(user) {
-    console.log("GOT USER: " + user.email)
+    if (user != null) console.log("GOT USER: " + user.email)
     res.json(user);
   })
 });
