d8a5c59757b796b2443c70abd2500e2480e84881
Vinnie Chen <vinnie.chen211@gmail.com>
2017-11-20 15:57:22 -0800

    implemented matches screen and connected it to messages screen, need to hook up to Twilio

--- react_app/screens/MatchesScreen.js
+++ react_app/screens/MatchesScreen.js
@@ -3,8 +3,33 @@ import {
   Text,
   Button,
   View,
-  StyleSheet
+  StyleSheet,
+	Image, ScrollView,
 } from 'react-native';
+import {
+	Card, ListItem, Icon
+} from 'react-native-elements'
+
+const users = [
+ {
+    name: 'brynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+	 message: "hey hot stuff"
+ },{
+    name: 'brandon',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+ },{
+    name: 'andrew',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+	 message: "hey hottie u down to be my thottie ;)"
+ },{
+    name: 'sarah',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+ },{
+    name: 'brynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+ },
+]
 
 class MatchesScreen extends Component {
   static navigationOptions = {
@@ -16,13 +41,30 @@ class MatchesScreen extends Component {
     const { navigate } = this.props.navigation;
 
     return (
-      <View>
+      <ScrollView>
       <Button
           onPress={() => navigate("Thread", {receiver: "insertuserhere"})}
           title="Message Thread"
           buttonStyle={{ marginTop: 20 }}
       />
-      </View>
+		<Card containerStyle={{padding: 0}} >
+  {
+    users.map((u, i) => {
+      return (
+        <ListItem
+          key={i}
+          roundAvatar
+          title={u.name}
+          avatar={{uri:u.avatar}}
+		  subtitle={u.message}
+			onPress={() => navigate("Thread", {receiver: "insertuserhere"})}
+        />
+      );
+    })
+  }
+</Card>
+
+      </ScrollView>
     );
   }
 }
