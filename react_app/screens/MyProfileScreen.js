fb1aacf11d150dcfa94d2f9bf048db0b7bf3c5f4
v2trang <vylanatrang@yahoo.com>
2017-11-23 10:30:14 -0800

    the actual screens

--- react_app/screens/MyProfileScreen.js
+++ react_app/screens/MyProfileScreen.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import {
   Text,
-  Button,
+  Image,
   View,
   StyleSheet
 } from 'react-native';
 
 //Method for logging out.
-import { onSignOut } from '../auth.js';
+import { onSignOut } from '../auth.js'
+import {Avatar} from 'react-native-elements'
+import {Icon} from 'react-native-elements'
+import {Button} from 'react-native-elements'
+import {Header} from 'react-native-elements'
+
+var profilePhoto = require('../images/bread.jpg');
 
 class MyProfileScreen extends Component {
 
@@ -17,22 +23,70 @@ class MyProfileScreen extends Component {
 
   render() {
 
+
     const { navigate } = this.props.navigation;
 
       return (
-        <View>
+
+       <View
+          style={{flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'}}
+        >
+
+        <Avatar
+        padding={10}
+        width={225}
+        height={225}
+        rounded
+        source={profilePhoto}
+        onPress={()=>navigate('UserProfile', {user:'yourOwnProfile'})}
+        activeOpacity={0.7}
+        />
+         <View
+          style={{flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center'}}
+        >
+        <Icon name='attach-money'/>
+        <Icon name='group'/>
+        <Icon name='build'/>
+        <Icon/>
+        <Icon/>
+
+        </View>
+        <Text>
+        IDEA/TITLE
+        </Text>
+
+        <Text>
+        NAME
+        </Text>
+
+        <Text>
+        Location
+        </Text>
+
         <Button
-            onPress={() => navigate("Explore", {user: "insertuserhere"})}
-            title="Explore Page"
-            buttonStyle={{ marginTop: 20 }}
+          rasied='true'
+          backgroundColor='#57C4DD'
+          icon={{name: 'settings'}}
+          title="Edit Account Settings"
+          onPress={() => navigate("Settings")}
         />
         <Button
-          buttonStyle={{ marginTop: 20 }}
-          backgroundColor="#03A9F4"
-          title="Log Out"
-          onPress={() => {
-            onSignOut().then(() => navigate("SignedOut"));
-          }}
+          backgroundColor='#57C4DD'
+          icon={{name: 'mode-edit'}}
+          title="Edit Profile"
+          onPress={() => navigate("Edit")}
+        />
+        <Button
+          title="Switch Profile"
+          backgroundColor='#57C4DD'
+          icon={{name: 'autorenew'}}
+          onPress={() => navigate("MyProfile")}
         />
         </View>
 
--- react_app/screens/UserProfileScreen.js
+++ react_app/screens/UserProfileScreen.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import {
   Text,
   Button,
+  Image,
   View,
   StyleSheet
 } from 'react-native';
+
+import Svg, { Circle } from 'react-native-svg';
+import {Avatar} from 'react-native-elements';
+import {Icon} from 'react-native-elements';
+
+var profilePhoto = require('../images/bread.jpg');
 
 class UserProfileScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -16,12 +23,122 @@ class UserProfileScreen extends Component {
     const { navigate } = this.props.navigation;
 
     return (
-      <View>
-      <Button
-          onPress={() => navigate("Explore", {user: "hiimauser"})}
-          title="Explore Page"
-          buttonStyle={{ marginTop: 20 }}
-      />
+      <View
+        style={{
+          flex:1,
+        flexDirection:'column' }}
+      >
+        <View
+        style={{flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'}}
+        >
+        <Avatar
+        padding={10}
+        width={225}
+        height={225}
+        rounded
+        source={profilePhoto}
+        activeOpacity={0.7}
+        />
+        <Text>
+        IDEA TITLE/NAME
+        </Text>
+
+        <Text>
+        This is a bio. It describes either the backer or the idea to back.
+        </Text>
+        </View>
+
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            //alignItems: 'center'
+          }}
+        >
+          {/*
+          <Icon name="attach-money" size={30}/>
+          <Icon name="open-book" type="entypo" size={30}/>
+          <Icon name="md-people" type="ionicon" size={30}/>
+          <Icon name="tools" type="entypo" size={30}/>
+           <Icon name="cubes" type="font-awesome" size={30}/> */}
+
+          <Svg height="50"
+                width="50"
+                style={{
+                  //alignItems: "center",
+                  justifyContent: 'center'
+                }}>
+               <Icon name="attach-money" size={30}/>
+               <Circle
+              cx="25"
+              cy="25"
+              r="25"
+              fill="white"/>
+          </Svg>
+
+          <Svg height="50"
+                width="50"
+                style={{
+                  //alignItems: "center",
+                  justifyContent: 'center'
+                }}>
+               <Icon name="open-book" type="entypo" size={30}/>
+               <Circle
+                            cx="25"
+                            cy="25"
+                            r="25"
+                            fill="white"/>
+          </Svg>
+
+          <Svg height="50"
+                width="50"
+                style={{
+                  //alignItems: "center",
+                  justifyContent: 'center'
+                }}>
+               <Icon name="md-people" type="ionicon" size={30}/>
+               <Circle
+                            cx="25"
+                            cy="25"
+                            r="25"
+                            fill='#3fd3df'/>
+          </Svg>
+
+          <Svg height="50"
+                width="50"
+                        style={{
+                          //alignItems: "center",
+                          justifyContent: 'center'
+                        }}>
+                       <Icon name="tools" type="entypo" size={30}/>
+                       <Circle
+                                    cx="25"
+                                    cy="25"
+                                    r="25"
+                                    fill="white"/>
+                  </Svg>
+
+                  <Svg height="50"
+                        width="50"
+                        style={{
+                          //alignItems: "center",
+                          justifyContent: 'center'
+                        }}>
+                       <Icon name="cubes" type="font-awesome" size={30}/>
+                       <Circle
+                                    cx="25"
+                                    cy="25"
+                                    r="25"
+                                    fill="#3fd3df"/>
+                      </Svg>
+
+
+
+        </View>
       </View>
     );
   }
