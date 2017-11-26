ed9bce1865438f633428147e1dd13363bf8fcddb
bpaw <bpaw.dev@gmail.com>
2017-11-25 21:11:23 -0800

    twilio api configuration

---
+++ twilio.js
@@ -0,0 +1,45 @@
+var twilio = require('twilio')
+var AccessToken = twilio.jwt.AccessToken;
+var ChatGrant = AccessToken.ChatGrant;
+
+function TokenProvider(credentials) {
+  Object.defineProperties(this, {
+    accountSid: {
+      enumerable: true,
+      value: credentials.accountSid
+    },
+    signingKeySid: {
+      enumerable: true,
+      value: credentials.signingKeySid
+    },
+    signingKeySecret: {
+      enumerable: true,
+      value: credentials.signingKeySecret || credentials.authToken
+    },
+    serviceSid: {
+      enumerable: true,
+      value: credentials.serviceSid || credentials.instanceSid
+    },
+    pushCredentialSid: {
+      enumerable: true,
+      value: credentials.pushCredentialSid
+    }
+  });
+}
+
+TokenProvider.prototype.getToken = function(identity, endpointId) {
+  var token = new AccessToken(this.accountSid, this.signingKeySid, this.signingKeySecret, {
+    identity: identity,
+    ttl: 40000
+  });
+
+  var grant = new ChatGrant({ pushCredentialSid: this.pushCredentialSid });
+
+  grant.serviceSid = this.serviceSid;
+  grant.endpointId = this.serviceSid + identity + endpointId;
+  token.addGrant(grant);
+
+  return token.toJwt();
+};
+
+module.exports = TokenProvider;
