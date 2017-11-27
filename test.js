23db17f478e6216e8666f0cf823c704f46c1a965
bpaw <bpaw.dev@gmail.com>
2017-11-27 13:52:58 -0800

    made client local to each route

--- test.js
+++ test.js
@@ -137,49 +137,49 @@ var url = 'http://localhost:8080/'
 // 	console.log("POST response body  - " + res.body);
 // });
 
-// var test_channel = {
-//   "description": "This is a test channel",
-//   "friendlyName": "tester2017",
-//   "uniqueName": "test_channel_fire",
-//   "identity" : "brandon",
-//   "endpointId": "61553df94c234a691130ab9d3438b074"
-// }
+var test_channel = {
+  "description": "This is a test channel",
+  "friendlyName": "tester2017",
+  "uniqueName": "channel2",
+  "identity" : "brandon",
+  "endpointId": "61553df94c234a691130ab9d3438b074"
+}
 
-// request.post({
-//     url: url + "twilio/channels",
-//     // method: "POST",
-//     json: true,   // <--Very important!!!
-//     body: test_channel
-// }, function(err, res) {
-//   if (err) {
-//     console.log(err)
-//   }
-// });
+request.post({
+    url: url + "twilio/channels",
+    // method: "POST",
+    json: true,   // <--Very important!!!
+    body: test_channel
+}, function(err, res) {
+  if (err) {
+    console.log(err)
+  }
+});
 
-// request.get({
-//   url: url + "twilio/channels?identity=brandon&endpointId=61553df94c234a691130ab9d3438b074",
-//   json: true   // <--Very important, otherwise it will be defaulted to HTML!!!
-// }, function(err, res) {
-//   if (res != null && res.body != null){
-//     console.log("The result is : " + JSON.stringify(res.body));
-//     token = res.body
-//   }
-// });
+request.get({
+  url: url + "twilio/channels?identity=brandon&endpointId=61553df94c234a691130ab9d3438b074",
+  json: true   // <--Very important, otherwise it will be defaulted to HTML!!!
+}, function(err, res) {
+  if (res != null && res.body != null){
+    console.log("The result is : " + JSON.stringify(res.body));
+    token = res.body
+  }
+});
 
-// var test_message = {
-//   "messageBody": "Vinay u is a dumb dumb"
-// }
+var test_message = {
+  "messageBody": "Vinay u is a dumb dumb"
+}
 
-// request.post({
-//     url: url + "twilio/channels/channel1/messages?identity=brandon&endpointId=61553df94c234a691130ab9d3438b074",
-//     // method: "POST",
-//     json: true,   // <--Very important!!!
-//     body: test_message
-// }, function(err, res) {
-//   if (err) {
-//     console.log(err)
-//   }
-// });
+request.post({
+    url: url + "twilio/channels/channel1/messages?identity=brandon&endpointId=61553df94c234a691130ab9d3438b074",
+    // method: "POST",
+    json: true,   // <--Very important!!!
+    body: test_message
+}, function(err, res) {
+  if (err) {
+    console.log(err)
+  }
+});
 
 request.get({
     url: url + "twilio/channels/channel1/messages?identity=brandon&endpointId=61553df94c234a691130ab9d3438b074",
