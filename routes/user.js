27f324dcdc1ebcee7c5b6c5a86adfc0fe4ac5fb7
Sarah Gemperle <sarah@sarahs-mbp.home>
2017-12-08 04:23:57 -0800

    changed body to query in user.js getPotentialMatches

--- routes/user.js
+++ routes/user.js
@@ -163,8 +163,8 @@ router.get('/settings', function(req, res) {
 });
 
 router.get('/getPotentialMatches', function(req, res){
-  var email = req.body.email
-  var isMaker = req.body.isMaker
+  var email = req.query && req.query.email
+  var email = req.query && req.query.isMaker
   db.getPotentialMatches(database,email,isMaker,function(user){
     if (user !== null) console.log("got potential matches")
       res.json(user)
