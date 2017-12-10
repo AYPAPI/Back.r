0df9a23ce3976db04582d43b10325396df372884
Sarah Gemperle <sarah@Sarahs-MacBook-Pro.local>
2017-12-10 04:57:54 -0800

    made changes to database

--- react_app/router/api.js
+++ react_app/router/api.js
@@ -150,7 +150,7 @@ exports.postSwipe = (email, swipedEmail, isMaker, swipedRight, name, swipedName)
       })
   }).then(function(response) {
     console.log("inside postSwipe api util callback");
-    return response.json();
+    return response;
   })
     .catch((error) => {
       console.error(error);
